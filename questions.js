d776be5177885a6b13d46c6bbb8442068af41a7f
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-03-29 19:46:29 -0400

    Wireframe Layout

---
+++ questions.js
@@ -0,0 +1,59 @@
+const STORE = [
+    {
+        question: 'How many pause weeks are available in the Thinkful Program?',
+        answers: ['0', '1', '4', '6'
+        ],
+        correctAnswer:
+            '6'
+    },
+    {
+        question:
+            'How many times is it possible to cry while learning coding?',
+        answers: ['0', '1', '15', '1000 or more'
+        ],
+        correctAnswer:
+            '1000 or more'
+    },
+    {
+        question:
+            'In javascript how do you access jQuery?',
+        answers: ['@', '#', '$', '.'],
+        correctAnswer: '$'
+    },
+    {
+        question: 'What are some recommended resources to use while in the Thinkful program ? ',
+        answers: ['Twitter', 'Snapchat', 'A Goldfish', 'Slack, W3 schools'
+        ],
+        correctAnswer: 'Slack, W3 schools'
+    },
+    {
+        question:
+            'Which of the following is the recommended way to write variables in javacript:',
+        answers: ['var', 'let', 'function', 'if'
+        ],
+        correctAnswer:
+            'let'
+    },
+    {
+        question: 'The _______ statement stops the execution of JavaScript, and calls the previous function if available:',
+        answers: ['callBack', 'Stopper', 'debugger', 'exStop'
+        ],
+        correctAnswer: 'debugger'
+    },
+    {
+        question:
+            'Which of the following is an example of a key/value pair?',
+        answers: ['let obj = {', 'name : “Taylor”', 'for (i = 0; i < cars.length; i++)', 'i = 22'
+        ],
+        correctAnswer:
+            'name : "Taylor"'
+    },
+    {
+        question: 'How do you write a for loop',
+        answers: ['for (loop = 0, i > name.length, i +)', 'for (i = 0; i < 5; i++)', 'for let i>= 0', 'There is no such thing as a "for loop"'
+        ],
+        correctAnswer:
+            'for (i = 0; i < 5; i++)'
+
+    }
+];
